954e337ab6d20e769fd7b5c51d969038f59235fa
victoirempkn <96738243+victoiremupikano@users.noreply.github.com>
2023-08-10 11:39:35 +0200

    connexion a une base de donnee specifique et visualisation des tous les tables qui y sont

--- to_mysql.py
+++ to_mysql.py
@@ -2,7 +2,7 @@
 # Now we will import that package
 import pymysql
 
-db = pymysql.connect(host="localhost", user="root", password="toor", port=3310)
+db = pymysql.connect(host="localhost", user="root", password="0000", port=3306)
 # you have cursor instance here
 cursor = db.cursor()
 # on print la version
@@ -16,4 +16,15 @@ print(len(data))
 print("La liste des bases de donnees :")
 for i in data:
     print(i)
-
+    
+# connection a la base de donne specifique
+dbd = pymysql.connect(host="localhost", user="root", password="0000", port=3306, database="sakila")
+cursor = dbd.cursor()
+# on recuper les bases de donnes qui sont
+cursor.execute("show tables")
+data = cursor.fetchall()
+print("La liste des tables de 'sakila' :")
+for i in data:
+    print(i)
+    
+    
